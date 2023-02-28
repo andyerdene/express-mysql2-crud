54023c4ecb8e1a1e5ad4336c6c562c9ede4aba28
andyerdene <anidas.com@gmail.com>
2023-02-28 16:53:43 +0800

    a

--- services/emp-service.js
+++ services/emp-service.js
@@ -1,4 +1,5 @@
 import { pool } from "../config/mysql-config.js";
+
 export async function getEmployees(limit) {
   const [rows] = await pool.query(`SELECT emp_no FROM salaries limit ${limit}`);
   return rows;
